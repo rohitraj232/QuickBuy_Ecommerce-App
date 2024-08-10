56589cf680bfc20c469deb2a644fca6261cea98c
Rohit Raj <rohitsp927@gmail.com>
2024-08-10 23:10:03 +0530

    added dynamic functionality to ProductDetail

--- src/components/admin/ProductDetail.jsx
+++ src/components/admin/ProductDetail.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import myContext from "../../context/myContext"
+import Loader from "../loader/Loader";
 
 const ProductDetail = () => {
+
+    const context = useContext(myContext);
+    const { loading, getAllProduct } = context;
+
+    // console.log(getAllProduct);
+
     return (
         <>
             <section>
@@ -12,27 +21,45 @@ const ProductDetail = () => {
                             </div>
                             <div className="col-6">
                                 <Link to="/add-product">
-                                <button type="button" class="btn btn-primary">Add Product</button>
+                                    <button type="button" class="btn btn-primary">Add Product</button>
                                 </Link>
                             </div>
                         </div>
+
+                        {/* loader component */}
+                        {loading && <Loader/>}
 
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
                                     <th scope="col">S.No.</th>
-                                    <th scope="col">Location Name</th>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">Date</th>
                                     <th scope="col">Action</th>
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Name</td>
-                                    <td>Edit</td>
-                                    <td>Delete</td>
-                                </tr>
+
+                                {getAllProduct.map((item, index) => {
+                                    const {id, title, price, category, date, productImageUrl} = item
+                                    return (
+                                        <tr key={index}>
+                                            <th scope="row">{index + 1}.</th>
+                                            <td><img width="50" src={productImageUrl} alt="product image" /></td>
+                                            <td>{title}</td>
+                                            <td>{price}</td>
+                                            <td>{category}</td>
+                                            <td>{date}</td>
+                                            <td>Edit</td>
+                                            <td>Delete</td>
+                                        </tr>
+                                    )
+                                })}
+
                             </tbody>
                         </table>
                     </div>
--- src/context/myState.jsx
+++ src/context/myState.jsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MyContext from "./myContext";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { fireDB } from "../firebase/FirebaseConfig";
 
 const myState = ({children}) => {
     // const name = "rohit raj"
     const [loading, setLoading] = useState(false);
+    
+    // get all product state
+    const [getAllProduct, setGetAllProduct] = useState([]);
+
+    // get all product function
+    const getAllProductFunction = async () => {
+      setLoading(true);
+
+      try {
+        const q = query(
+          collection(fireDB, 'product'),
+          orderBy('time')
+        )
+        const data = onSnapshot(q, (QuerySnapshot) => {
+          let productArray = [];
+          QuerySnapshot.forEach((doc) => {
+            productArray.push({...doc.data(), id: doc.id});
+          });
+          setGetAllProduct(productArray);
+          setLoading(false);
+        });
+        return () => data;
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+
+
+    useEffect(() => {
+      getAllProductFunction();
+    }, []);
+
   return (
     <MyContext.Provider value={{
         loading,
-        setLoading
+        setLoading,
+        getAllProduct
     }}>
       {children}
     </MyContext.Provider>
