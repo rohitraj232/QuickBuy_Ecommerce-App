39bbaa9b3fabba1e780dd5ba7dffdc482c823fa9
Rohit Raj <rohitsp927@gmail.com>
2024-08-17 00:08:59 +0530

    installed redux-toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS (includes Popper.js)
+import { Provider } from 'react-redux';
+import { store } from './redux/store.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
